121eb8283d1e644bb13d23248ac13fcc75d05b00
MURALI <mummadimuralimohanreddy@gmail.com>
2025-03-11 00:15:43 +0530

    Managed APIKey and removed debug banner

--- lib/core/api_constants.dart
+++ lib/core/api_constants.dart
@@ -1,5 +1,5 @@
 class ApiConstants{
-  static const apiKey = "f42d99af7c44400fa1ef852dfcd3c3df";
+  static const apiKey = "APIKEY";
   static const everything="https://newsapi.org/v2/everything?domains=bbc.co.uk&sortBy=popularity&apiKey=$apiKey";
   static const topHeadLines="https://newsapi.org/v2/top-headlines?country=us&apiKey=$apiKey";
   static const popular="https://newsapi.org/v2/everything?q=apple&from=2025-02-24&to=2025-02-24&sortBy=popularity&apiKey=$apiKey";
--- lib/main.dart
+++ lib/main.dart
@@ -33,6 +33,7 @@ class MyApp extends StatelessWidget {
              ),
            ],
            child: MaterialApp(
+             debugShowCheckedModeBanner: false,
              title: 'News Digest',
              theme: ThemeData(
                // This is the theme of your application.
